6ef86e3093d8e8e82e2bad984c53899eed7dc6c1
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-07-16 23:21:50 -0600

    Up to Step 5.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,25 @@ import axios from 'axios';
 
 class App extends Component {
  
+
+  componentDidMount(){
+    axios.get('/api/messages').then( (res) => {
+      this.setState({
+        allMessages: res.data
+      })
+    })
+  }
+
+  createMessage(){
+    let {username, message} = this.state;
+    axios.post('/api/messages', {username: username, message: message}).then( (res) => {
+      this.setState({
+          allMessages: res.data
+      })
+    })
+  }
+
+
   render(){
     return (
       <p> Hello World </p>
@@ -13,3 +32,9 @@ class App extends Component {
 }
 
 export default App;
+
+// Find the createMessage method and make a post request. Send this.state.username and this.state.message in the body of the request. Use username and message property names.
+// path: '/api/messages'
+// body: {username: this.state.username, message: this.state.message}
+// Set state with the response (which will be the updated array messages from the server)
+// Update the allMessages property.
