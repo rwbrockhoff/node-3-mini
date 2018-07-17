556115cd1855ad011ab57ca9c6e83eb0d70bfc03
Ryan Brockhoff <brockhoffrw@gmail.com>
2018-07-16 23:17:11 -0600

    Started working into the App. Server set up. Controller has get, post.

--- server/server.js
+++ server/server.js
@@ -1,39 +1,23 @@
-require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const messagesCtrl = require('./messagesCtrl');
-const session = require('express-session');
+require('dotenv').config();
+const port = process.env.SERVER_PORT;
 
-let { SERVER_PORT, SESSION_SECRET } = process.env;
+const messagesCtrl = require('./messagesCtrl');
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(
-  session({
-    secret: SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-  })
-);
-app.use((req, res, next) => {
-  let badWords = ['knucklehead', 'jerk', 'internet explorer'];
-  if (req.body.message) {
-    let badWordsExist = true;
-    for (let i = 0; i < badWords.length; i++) {
-      let regex = new RegExp(badWords[i], 'g');
-      req.body.message = req.body.message.replace(regex, '****');
-    }
-    next();
-  } else {
-    next();
-  }
-});
+app.listen(port, () => console.log(`Making all the magic happen at port: ${port}`));
+
 
 app.get('/api/messages', messagesCtrl.getAllMessages);
-app.get('/api/messages/history', messagesCtrl.history);
 app.post('/api/messages', messagesCtrl.createMessage);
 
-app.listen(SERVER_PORT, () => {
-  console.log(`Server listening on port ${SERVER_PORT}.`);
-});
+
+
+
+// Create a POST endpoint with a path of /api/messages and use the createMessage method as the callback.
+
+
+
